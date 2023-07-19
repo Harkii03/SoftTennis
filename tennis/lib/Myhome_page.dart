de5b0599362424cd1620289ea55ca54fcf75ffa9
Hr__03.s <har.kii0324@gmail.com>
2023-07-19 10:27:11 +0900

    add login function and modified UI

--- tennis/lib/Myhome_page.dart
+++ tennis/lib/Myhome_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:tennis/Record_page.dart';
+import 'package:tennis/Event_page.dart';
+import 'package:tennis/Video_page.dart';
+import 'package:tennis/Login_page.dart';
+
+import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'Video_page.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 // 会津若松体育施設予約のURL
 final Uri _url = Uri.parse('https://reserve.city.aizuwakamatsu.fukushima.jp/');
@@ -17,196 +23,175 @@ Future<void> _launchUrl() async {
 DateTime _focused = DateTime.now();
 DateTime? _selected;
 
-class EventModel {
-  final DateTime date;
-  final String title;
-
-  EventModel({required this.date, required this.title});
-}
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+  const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePage();
 }
 
 class _MyHomePage extends State<MyHomePage> {
-  //イベントのリストア
-  List<EventModel> events = [
-    EventModel(date: DateTime(2023, 6, 1), title: 'イベント1'),
-    EventModel(date: DateTime(2023, 6, 5), title: 'イベント2'),
-    EventModel(date: DateTime(2023, 6, 15), title: 'イベント3'),
-    // 他のイベントを追加...
-  ];
+  //仮のデータ
+  final sampleEvents = {
+    DateTime.utc(2023, 8, 3): ['firstEvent', 'secodnEvent'],
+    DateTime.utc(2023, 8, 5): ['thirdEvent', 'fourthEvent']
+  };
+  List<String> _selectedEvents = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("ソフトテニス管理アプリ",
-            style: TextStyle(color: Color.fromARGB(246, 241, 205, 172))),
-        backgroundColor: const Color.fromARGB(173, 49, 44, 44),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute<void>(
-                    builder: (BuildContext context) => RecordPage()),
-              );
-            },
-            child: Text("イベントを追加",
-                style: TextStyle(
-                  color: Color.fromARGB(246, 222, 222, 222),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 11.0,
-                  decoration: TextDecoration.underline,
-                )),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(16.0),
-            //縁取り
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Color.fromARGB(246, 241, 205, 172),
-                width: 2.0,
-              ),
-              borderRadius: BorderRadius.circular(50.0),
+        appBar: AppBar(
+          title: const Text("ソフトテニス管理アプリ",
+              style: TextStyle(color: Color.fromARGB(246, 241, 205, 172))),
+          backgroundColor: const Color.fromARGB(173, 49, 44, 44),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context) => EventPage()),
+                );
+              },
+              child: const Text("イベントを追加",
+                  style: TextStyle(
+                    color: Color.fromARGB(246, 222, 222, 222),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 11.0,
+                    decoration: TextDecoration.underline,
+                  )),
             ),
-            child: SizedBox(
-              width: double.infinity,
-              height: 395.0,
-              //カレンダ0の表示
-              child: TableCalendar(
-                firstDay: DateTime.utc(2022, 4, 1),
-                lastDay: DateTime.utc(2025, 12, 31),
-                selectedDayPredicate: (day) {
-                  return isSameDay(_selected, day);
+          ],
+        ),
+        body: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2022, 4, 1),
+              lastDay: DateTime.utc(2025, 12, 31),
+              locale: 'ja_JP',
+              eventLoader: (date) {
+                return sampleEvents[date] ?? [];
+              },
+              selectedDayPredicate: (day) {
+                return isSameDay(_selected, day);
+              },
+              onDaySelected: (selected, focused) {
+                if (!isSameDay(_selected, selected)) {
+                  setState(() {
+                    _selected = selected;
+                    _focused = focused;
+                    _selectedEvents = sampleEvents[selected] ?? [];
+                  });
+                }
+              },
+              focusedDay: _focused,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _selectedEvents.length,
+                itemBuilder: (context, index) {
+                  final event = _selectedEvents[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(event),
+                    ),
+                  );
                 },
-                onDaySelected: (selected, focused) {
-                  if (!isSameDay(_selected, selected)) {
-                    setState(() {
-                      _selected = selected;
-                      _focused = focused;
-                    });
-                  }
-                },
-                focusedDay: _focused,
               ),
             ),
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        // 枠線
-                        border: Border.all(
-                            color: Color.fromARGB(246, 241, 205, 172),
-                            width: 2),
-                        // 角丸
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      width: 200,
-                      height: 70,
-                      child: OutlinedButton(
-                        child: const Text('記録'),
-                        style: OutlinedButton.styleFrom(
-                          primary: Colors.black,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute<void>(
-                                builder: (BuildContext context) =>
-                                    RecordPage()),
-                          );
-                        },
-                      ),
-                    ),
-                    //余白
-                    SizedBox(height: 10),
-                    Container(
-                      decoration: BoxDecoration(
-                        // 枠線
-                        border: Border.all(
-                          color: Color.fromARGB(246, 241, 205, 172),
-                          width: 2,
-                        ),
-                        // 角丸
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      width: 200,
-                      height: 70,
-                      child: OutlinedButton(
-                          child: const Text('コートの予約'),
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.black,
-                          ),
-                          onPressed: () {
-                            // 予約ページに遷移する処理
-                            //'https://reserve.city.aizuwakamatsu.fukushima.jp/' に飛ぶ
-                            _launchUrl();
-                          }),
-                    ),
-                    //余白
-                    SizedBox(height: 10),
-                    Container(
-                      decoration: BoxDecoration(
-                        // 枠線
-                        border: Border.all(
-                            color: Color.fromARGB(246, 241, 205, 172),
-                            width: 2),
-                        // 角丸
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      width: 200,
-                      height: 70,
-                      child: OutlinedButton(
-                        child: const Text('動画'),
-                        style: OutlinedButton.styleFrom(
-                          primary: Colors.black,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute<void>(
-                                builder: (BuildContext context) => VideoPage()),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromARGB(246, 241, 205, 172),
+                  width: 2,
                 ),
-                Container(
-                  margin: EdgeInsets.all(8.0),
-                  width: 150,
-                  height: 230,
-                  decoration: BoxDecoration(
-                    // 枠線
-                    border: Border.all(
-                        color: Color.fromARGB(246, 241, 205, 172), width: 2),
-                    // 角丸
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Text('イベントを表示をさせる'),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: 200,
+              height: 70,
+              child: OutlinedButton(
+                child: const Text('コートの予約'),
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.black,
                 ),
-              ],
+                onPressed: () {
+                  // 予約ページに遷移する処理
+                  // 'https://reserve.city.aizuwakamatsu.fukushima.jp/' に飛ぶ
+                  _launchUrl();
+                },
+              ),
             ),
-          ),
-        ],
-      ),
-      backgroundColor: Color.fromARGB(238, 239, 232, 201),
-    );
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromARGB(246, 241, 205, 172),
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: 200,
+              height: 70,
+              child: OutlinedButton(
+                child: const Text('ログアウト'),
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+                onPressed: () async {
+                  // ログアウト処理
+                  // 内部で保持しているログイン情報等が初期化される
+                  // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
+                  await FirebaseAuth.instance.signOut();
+                  // ログイン画面に遷移＋チャット画面を破棄
+                  await Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) {
+                      return LoginPage();
+                    }),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'ホーム',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book),
+              label: '記録',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.video_call),
+              label: '動画',
+            ),
+          ],
+          onTap: (index) {
+            switch (index) {
+              case 0:
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context) => MyHomePage()),
+                );
+                break;
+              case 1:
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context) => RecordPage()),
+                );
+                break;
+              case 2:
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context) => VideoPage()),
+                );
+                break;
+            }
+          },
+        ));
   }
 }
