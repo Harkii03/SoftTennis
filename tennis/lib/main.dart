95dbef9944a11484753bd7a64bfc7138e849fc9e
Hr__03.s <har.kii0324@gmail.com>
2023-07-19 10:27:34 +0900

    add Login function

--- tennis/lib/main.dart
+++ tennis/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:tennis/Myhome_page.dart';
+import 'package:tennis/Login_page.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
+  // Firebaseの初期化
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // 日本語化
+  initializeDateFormatting('ja_JP');
   runApp(const MyApp());
 }
 
@@ -16,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'SoftTenis App'),
+      home: LoginPage(),
     );
   }
 }
