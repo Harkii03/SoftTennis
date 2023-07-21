3f0ddc65e672918df5aa7c3622d1a492a74973e1
Hr__03.s <har.kii0324@gmail.com>
2023-07-21 10:18:53 +0900

    add menu function

--- tennis/lib/Screens/Video_page.dart
+++ tennis/lib/Screens/Video_page.dart
@@ -5,10 +5,20 @@ import 'package:youtube_api/youtube_api.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // 予約ページに遷移する処理
+// Future<void> _launchUrl(url) async {
+//   Uri _url = Uri.parse(url);
+//   if (!await launchUrl(_url)) {
+//     throw Exception('Could not launch $_url');
+//   }
+// }
+
+//実機用(Youtube appに飛ぶ)
 Future<void> _launchUrl(url) async {
   Uri _url = Uri.parse(url);
-  if (!await launchUrl(_url)) {
+  if (!await canLaunchUrl(_url)) {
     throw Exception('Could not launch $_url');
+  } else {
+    await launch(_url.toString(), forceSafariVC: false);
   }
 }
 
@@ -19,10 +29,14 @@ class VideoPage extends StatefulWidget {
 }
 
 class _VideoPageState extends State<VideoPage> {
+  bool isCollapsed = true;
+  double screenWidth = 0, screenHeight = 0;
+  final Duration duration = const Duration(milliseconds: 300);
+
   // YoutubeAPIのキー
   static String key = "";
 
-  YoutubeAPI youtube = YoutubeAPI(key);
+  YoutubeAPI youtube = YoutubeAPI(key, maxResults: 20, type: "video");
   List<YouTubeVideo> videoResult = [];
 
   Future<void> callAPI() async {
@@ -40,36 +54,69 @@ class _VideoPageState extends State<VideoPage> {
   void initState() {
     super.initState();
     callAPI();
-    print('hello');
+    print('sucressed to get video data');
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
     return Scaffold(
-      appBar: AppBar(
-        // タイトル
-        title: const Text('動画ページ',
-            style: TextStyle(color: Color.fromARGB(246, 241, 205, 172))),
-        // 背景色
-        backgroundColor: Color.fromARGB(173, 49, 44, 44),
-      ),
-      body: Column(children: [
-        Container(
-          child: Text(
-            "おすすめの動画",
-            style: TextStyle(color: Color.fromARGB(132, 0, 0, 0)),
+        appBar: AppBar(
+          // タイトル
+          title: const Text('動画ページ',
+              style: TextStyle(color: Color.fromARGB(246, 241, 205, 172))),
+          // 背景色
+          backgroundColor: const Color.fromARGB(173, 49, 44, 44),
+          leading: InkWell(
+            onTap: () {
+              setState(() {
+                isCollapsed = !isCollapsed;
+              });
+            },
+            child: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
           ),
         ),
+        body: Stack(
+          children: <Widget>[
+            menu(),
+            mainpage(context),
+          ],
+        ));
+  }
 
-        // 動画一覧を表示するウィジェットを追加
-        Expanded(
-          child: ListView(
-            children: videoResult.map<Widget>(listItem).toList(),
-          ),
-        ),
-      ]),
-    );
+  Widget mainpage(context) {
+    return AnimatedPositioned(
+        duration: duration,
+        top: isCollapsed ? 0 : 0,
+        bottom: isCollapsed ? 0 : 0,
+        left: isCollapsed ? 0 : 0.4 * screenWidth,
+        right: isCollapsed ? 0 : -0.4 * screenWidth,
+        child: Material(
+            animationDuration: duration,
+            elevation: 8,
+            color: Color.fromARGB(193, 241, 206, 166),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    "おすすめの動画",
+                    style: TextStyle(color: Color.fromARGB(132, 0, 0, 0)),
+                  ),
+                ),
+                // 動画一覧を表示するウィジェット
+                Expanded(
+                  child: ListView(
+                    children: videoResult.map<Widget>(listItem).toList(),
+                  ),
+                ),
+              ],
+            )));
   }
 
   Widget listItem(YouTubeVideo video) {
@@ -89,6 +136,7 @@ class _VideoPageState extends State<VideoPage> {
                   child: Image.network(
                     video.thumbnail.small.url ?? '',
                     width: 140.0,
+                    fit: BoxFit.cover,
                   ),
                 ),
                 Expanded(
@@ -115,7 +163,105 @@ class _VideoPageState extends State<VideoPage> {
             ),
           )),
     );
+  }
 
-    ;
+  Widget menu() {
+    return Container(
+      color: Color.fromARGB(173, 49, 44, 44),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            // メニューの項目を GestureDetector でラップし、onTapで選択された項目を処理
+            GestureDetector(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.white, width: 1.0),
+                  ),
+                ),
+                child: const Text(
+                  'メニュー           ',
+                  style: TextStyle(
+                      color: Color.fromARGB(246, 241, 205, 172), fontSize: 22),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 1.0,
+              ),
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.white, width: 1.0),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                // TODO: Singlesがタップされたときの処理
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                ),
+                child: const Text(
+                  'Singles                ',
+                  style: TextStyle(
+                      color: Color.fromARGB(246, 241, 205, 172), fontSize: 20),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                // TODO: Doublesがタップされたときの処理
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                ),
+                child: const Text(
+                  'Doubles               ',
+                  style: TextStyle(
+                      color: Color.fromARGB(246, 241, 205, 172), fontSize: 20),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                // TODO: Mixd Doublesがタップされたときの処理
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 9),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                ),
+                child: const Text(
+                  'Mix Doubles      ',
+                  style: TextStyle(
+                      color: Color.fromARGB(246, 241, 205, 172), fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
