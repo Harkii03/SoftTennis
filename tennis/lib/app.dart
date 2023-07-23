bdb2f706675df506a9953a4d01afc0d4e5b59cee
Hr__03.s <har.kii0324@gmail.com>
2023-07-23 23:49:35 +0900

    modified bottomnavicationbar

--- tennis/lib/app.dart
+++ tennis/lib/app.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tennis/Screens/Home_page.dart';
 import 'package:tennis/Screens/Record_page.dart';
-import 'package:tennis/Screens/Video_page.dart';
+import 'package:tennis/Screens/app_Video_page.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+
+// 会津若松体育施設予約のURL
+final Uri _url = Uri.parse('https://reserve.city.aizuwakamatsu.fukushima.jp/');
 
 class AppPage extends StatefulWidget {
   const AppPage({Key? key}) : super(key: key);
@@ -11,12 +16,24 @@ class AppPage extends StatefulWidget {
 }
 
 class _AppPage extends State<AppPage> {
-  static const _screens = [HomePage(), RecordPage(), VideoPage()];
+  static const _screens = [HomePage(), RecordPage(), null, AppVideoPage()];
   int _selectedIndex = 0;
+
+  // 予約ページに遷移する処理
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      if (index == 2) {
+        // If the "予約" item is pressed, launch the URL
+        _launchUrl();
+      } else {
+        _selectedIndex = index;
+      }
     });
   }
 
@@ -38,6 +55,10 @@ class _AppPage extends State<AppPage> {
                 icon: Icon(Icons.menu_book,
                     color: Color.fromARGB(246, 241, 205, 172)),
                 label: '記録'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book,
+                    color: Color.fromARGB(246, 241, 205, 172)),
+                label: '予約'),
             BottomNavigationBarItem(
                 icon: Icon(Icons.video_call,
                     color: Color.fromARGB(246, 241, 205, 172)),
