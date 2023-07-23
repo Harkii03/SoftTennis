ae01bb667b0c6b76c4d00460baf04b73b42193e7
Hr__03.s <har.kii0324@gmail.com>
2023-07-23 23:45:09 +0900

    For add event page

--- tennis/lib/Screens/Event/Event_page.dart
+++ tennis/lib/Screens/Event/Event_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
+import 'package:tennis/Screens/Event/Event_edit_page.dart';
 
 class EventPage extends StatefulWidget {
   @override
@@ -13,9 +15,92 @@ class _EventPageState extends State<EventPage> {
   final _eventNameController = TextEditingController();
   final _eventDescriptionController = TextEditingController();
 
+  int _selectedHour = 0; // 追加: 選択した時間を格納する変数
+  int _selectedMinute = 0; // 追加: 選択した分を格納する変数
+
   DateTime? _startDate;
 
   String _infoText = '';
+
+  String formatDate(DateTime date) {
+    return DateFormat('yyyy年MM月dd日').format(date);
+  }
+
+  void _selectStartDate() {
+    DatePicker.showDatePicker(
+      context,
+      currentTime: _startDate ?? DateTime.now(),
+      minTime: DateTime(2021),
+      maxTime: DateTime(2030),
+      locale: LocaleType.jp, // Set the locale for Japanese format
+      onConfirm: (DateTime picked) {
+        if (picked != null) {
+          setState(() {
+            _startDate = picked;
+          });
+        }
+      },
+    );
+  }
+
+  // 追加: 時間を選択するための関数
+  void _selectTime() {
+    DatePicker.showPicker(
+      context,
+      showTitleActions: true,
+      pickerModel: TimePickerModel(),
+      onConfirm: (time) {
+        if (time != null) {
+          setState(() {
+            _selectedHour = time.hour;
+            _selectedMinute = time.minute;
+          });
+        }
+      },
+    );
+  }
+
+  void _saveEvent() async {
+    try {
+      // Access the "events" collection in Firestore
+      CollectionReference eventsCollection =
+          FirebaseFirestore.instance.collection('events');
+
+      // Create a new event document with a unique ID
+      DocumentReference newEventRef = eventsCollection.doc();
+
+      // Get the current user's ID
+      var _user = FirebaseAuth.instance.currentUser;
+      String userId = _user?.uid.toString() ?? '';
+
+      // Format the _startDate to "YYYY-MM-DD" before saving to Firestore
+      String formattedDate =
+          DateFormat('yyyy-MM-dd').format(_startDate ?? DateTime.now());
+
+      // Update the event data in Firestore with a unique name (e.g., using timestamp)
+      String uniqueEventName = DateTime.now().toIso8601String();
+
+      await newEventRef.set({
+        'userId': userId,
+        'eventName': _eventNameController.text,
+        'eventDescription': _eventDescriptionController.text,
+        'eventDate': formattedDate, // Save the formatted date
+        'eventNameUnique': uniqueEventName, // Add the unique event name
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      setState(() {
+        _infoText = 'イベントを追加しました';
+      });
+
+      Future.delayed(Duration(seconds: 2), () {
+        Navigator.of(context).pop();
+      });
+    } catch (e) {
+      // Handle any errors that occurred during saving
+      print('Error saving event: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,20 +139,18 @@ class _EventPageState extends State<EventPage> {
                 },
               ),
               SizedBox(height: 20),
-              // ... Existing TextFormField widgets ...
 
               // Button to select start date
               ElevatedButton(
                 onPressed: _selectStartDate,
-                child: Text(_startDate != null
-                    ? '日付: ${_startDate?.toLocal()}'
-                    : '開始日を選択'),
+                child: Text(
+                  _startDate != null ? '日付: ${formatDate(_startDate!)}' : '日時',
+                ),
               ),
-
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _saveEvent, // Update onPressed to call _saveEvent
-                child: Text('イベントを追加'),
+                child: const Text('イベントを追加'),
               ),
               // メッセージ表示用のテキストウィジェット
               Text(_infoText),
@@ -76,59 +159,5 @@ class _EventPageState extends State<EventPage> {
         ),
       ),
     );
-  }
-
-  void _selectStartDate() {
-    DatePicker.showDatePicker(
-      context,
-      currentTime: _startDate ?? DateTime.now(),
-      minTime: DateTime(2021),
-      maxTime: DateTime(2030),
-      onConfirm: (DateTime picked) {
-        if (picked != null) {
-          setState(() {
-            _startDate = picked;
-          });
-        }
-      },
-    );
-  }
-
-  void _saveEvent() async {
-    try {
-      // Access the "events" collection in Firestore
-      CollectionReference eventsCollection =
-          FirebaseFirestore.instance.collection('events');
-
-      // Create a new event document with a unique ID
-      DocumentReference newEventRef = eventsCollection.doc();
-
-      // Get the current user's ID
-      var _user = FirebaseAuth.instance.currentUser;
-
-      String userId = _user?.uid.toString() ?? '';
-
-      // Save the event data to Firestore
-
-      await newEventRef.set({
-        'userId': userId,
-        'eventName': _eventNameController.text, // Use directly from form fields
-        'eventDescription':
-            _eventDescriptionController.text, // Use directly from form fields
-        'eventDate': _startDate,
-        'timestamp': FieldValue.serverTimestamp(),
-      });
-
-      setState(() {
-        _infoText = 'イベントを追加しました';
-      });
-
-      Future.delayed(Duration(seconds: 2), () {
-        Navigator.of(context).pop();
-      });
-    } catch (e) {
-      // Handle any errors that occurred during saving
-      print('Error saving event: $e');
-    }
   }
 }
